7d063d10f7d28cc2c3a1e346dfc0d885858a2c12
sunil321706 <167359492+sunil321706@users.noreply.github.com>
2024-04-18 17:53:09 +0000

    variable first step

---
+++ basic/variable.js
@@ -0,0 +1,15 @@
+const accountId = 98765
+let accountEmail = "sunil@gmail.com"
+var accountPassword = "1234"
+accountCity = "punjab"
+let accountState
+
+
+
+accountEmail = "sk@gmail.com"
+accountPassword = "8888"
+accountCity = "delhi"
+
+console.log(accountEmail)
+
+console.table({accountEmail, accountPassword, accountCity, accountState})
