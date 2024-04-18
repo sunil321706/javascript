a888e0eb9d2463052ddce2d754a03074e0cad2c1
sunil321706 <167359492+sunil321706@users.noreply.github.com>
2024-04-18 18:46:28 +0000

    datatype javascript

---
+++ basic/datatype.js
@@ -0,0 +1,28 @@
+"use String"; // treat all js code as newer version
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log(3
++
+3) // code readability should be high
+
+console.log("sunil")
+
+
+let name = "sunil"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value / object
+// undefined => undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof age);
