3e4b9b891c4d0a5c01305a8fb3a96fdff07be96a
sunil321706 <167359492+sunil321706@users.noreply.github.com>
2024-04-19 19:04:29 +0000

    convertion and datatypes

--- basic/conversonandvariable.js
+++ basic/conversonandvariable.js
@@ -1,4 +1,4 @@
-  let score = "sunil"
+   let score = "sunil"
 
 //   console.log(typeof score)
 //   console.log(typeof (score));
@@ -24,7 +24,44 @@ let booleanInLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// ***********************Operations**************************************
 
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "sunil"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// link to study
