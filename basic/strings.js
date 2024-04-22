7442dc22fe44b258c48ae91aa05d2c0670b05606
sunil321706 <167359492+sunil321706@users.noreply.github.com>
2024-04-22 17:20:49 +0000

    strings in java script

--- basic/strings.js
+++ basic/strings.js
@@ -1,6 +1,11 @@
 const name = "sunil"
 const count = 100
 
-console.log(name + count);
+// console.log(name + count);
 
-console.log(`this is a ${name} and this is a count ${count}`);
+// console.log(`this is a ${name} and this is a count ${count}`);
+
+const carName = "volvo"
+
+console.log(carName.length);
+
