52c6edeff1bae1f266c5ab717d0e692ef3c52489
sunil321706 <167359492+sunil321706@users.noreply.github.com>
2024-04-23 17:21:35 +0000

    strings in java script

--- basic/strings.js
+++ basic/strings.js
@@ -1,10 +1,25 @@
-const name = "sunil"
+const name = "this is a beautifull car"
 const count = 100
 
 // console.log(name + count);
 
 // console.log(`this is a ${name} and this is a count ${count}`);
 
-const carName = " sandeep "
-const trimedname = carName.trim()
-console.log(trimedname.endsWith("p"));
+// const carName = " sandeep "
+// const trimedname = carName.trim()
+// console.log(trimedname.endsWith("p"));
+
+
+// console.log(name.search("car"))
+// console.log("name's");
+
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+
+// console.log(name.charAt(2));
+
+// console.log(name.indexOf("a"));
+
+// console.log(name.split("a"));
+
+// console.log(name.slice(4, -4));
