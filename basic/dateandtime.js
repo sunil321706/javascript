0ac3d235f47e152c8fd9bc0d9ff3b2676d6de0d7
sunil321706 <167359492+sunil321706@users.noreply.github.com>
2024-04-26 18:24:25 +0000

    date and time

---
+++ basic/dateandtime.js
@@ -0,0 +1,9 @@
+let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.getTimezoneOffset());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleTimeString());
+
+console.log(typeof myDate);
