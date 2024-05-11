fe898e159de4efe1f4ed46f9d664c6a49e1effda
sunil321706 <167359492+sunil321706@users.noreply.github.com>
2024-05-11 09:36:16 +0000

    reverse a word

---
+++ program/reverse_a_word.js
@@ -0,0 +1,8 @@
+let sentence = "this is a book"
+console.log(sentence)
+const words = sentence.split(" ")
+console.log(words)
+const reversestring = words.reverse()
+console.log(reversestring)
+const reverseword = reversestring.join(" ");
+console.log(reverseword)
