3be73b053bbbf0d4970379ff0599de29472a5f4f
sunil321706 <167359492+sunil321706@users.noreply.github.com>
2024-05-17 19:50:43 +0000

    table in javascript

--- program/table.js
+++ program/table.js
@@ -1,6 +1,5 @@
 let x = parseInt(prompt("number"))
-let i = 1
-for(i <= 10) {
- console.log(x + " * " + i + " = " + (x * i));
- i++;
+for(let i=1; i <= 10; i++){
+ console.log(x + " * " + i + " = " + (x * i))
 }
+
