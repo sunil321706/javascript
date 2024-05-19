eca667b65eb66bde263c612e7eb6444bf64206a4
sunil321706 <167359492+sunil321706@users.noreply.github.com>
2024-05-19 09:27:30 +0000

    object two in javascript

--- 02_basic/03_object.js
+++ 02_basic/03_object.js
@@ -3,10 +3,11 @@
 
 const mySym = Symbol("key1")
 
+
 const jsUser = {
   name: "sunil",
   "full name": "sunil kumar",
-  mysym: "mykey1",
+  [mySym]: "mykey1",
   age: 18,
   location: "punjab",
   email: "sunil@google.com",
@@ -14,11 +15,22 @@ const jsUser = {
   lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(jsUser.email);
-console.log(jsUser["email"]);
-console.log(jsUser["full name"]);
-console.log(typeof jsUser.mysym);
-console.log(jsUser["mysym"]);
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+// console.log(jsUser["full name"]);
+// console.log(jsUser[mySym]); 
 
 jsUser.email = "sunil@chatgpt.com"
-Object.freeze(jsUser)
+// Object.freeze(jsUser)
+jsUser.email = "sunil@microsoft.com"
+// console.log(jsUser);
+
+jsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+jsUser.greetingTwo = function(){
+  console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
