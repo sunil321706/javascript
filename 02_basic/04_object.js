72360f370bf8ce32ab8fec8c96924efdeb956e3f
sunil321706 <167359492+sunil321706@users.noreply.github.com>
2024-05-19 09:58:12 +0000

    object 3 inn javascript

--- 02_basic/04_object.js
+++ 02_basic/04_object.js
@@ -21,7 +21,35 @@ const regularUser = {
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
 
 // const obj3 = {obj1, obj2}
-const obj3 = Object.assign({}, obj1, obj2)
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
 console.log(obj3);
+
+const user = [
+    {
+        id: 1,
+        email: "s@gmail.com"
+    },
+    {
+        id: 1,
+        email: "s@gmail.com"
+    }, 
+    {
+        id: 1,
+        email: "s@gmail.com"
+    },
+]
+
+user[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty(`isLoggedIn`));
