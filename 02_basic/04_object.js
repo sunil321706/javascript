49cec18f861e8e5b2e73db4f8089bbe6bf757cb9
sunil321706 <167359492+sunil321706@users.noreply.github.com>
2024-05-19 09:45:07 +0000

    object in javascript

---
+++ 02_basic/04_object.js
@@ -0,0 +1,27 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "sunil"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "sunil",
+            lastname: "kumar"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2}
+const obj3 = Object.assign({}, obj1, obj2)
+console.log(obj3);
