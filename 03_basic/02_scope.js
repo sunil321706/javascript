1e53b72f6bfea6a1f74a02a940dc9028ba8e94db
sunil321706 <167359492+sunil321706@users.noreply.github.com>
2024-05-19 17:53:17 +0000

    pfjfk

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -1,5 +1,11 @@
-let a = 10
-const b = 20
-var c = 30
+// var c = 300
+let a = 300
+if(true){
+    let a = 10
+    const b = 20
+    console.log("INNER: ", a);
+}
 
-console.log();
+console.log(a);
+// console.log(b);
+// console.log(c);
