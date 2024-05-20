522e5713ab6cb64f77fb8b0938ac8e1d331791c8
sunil321706 <167359492+sunil321706@users.noreply.github.com>
2024-05-20 17:22:12 +0000

    kmregrm

---
+++ 05_itteration/four.js
@@ -0,0 +1,25 @@
+const myobject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myobject) {
+    // console.log(`${key} shortcut is for ${myobject[key]}`);
+}
+
+const programming = ["ja", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+//    console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set(`IN`, "India")
+// map.set(`USA`, "United States of America")
+// map.set(`Fr`, "France")
+
+// for (const key in map) {
+//     console.log(key);
+// }
