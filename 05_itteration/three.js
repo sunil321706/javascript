27b173ecc3209df67c7b08caa5f278b79f3d23a6
sunil321706 <167359492+sunil321706@users.noreply.github.com>
2024-05-20 17:10:11 +0000

    krmfkfr

--- 05_itteration/three.js
+++ 05_itteration/three.js
@@ -4,3 +4,34 @@
 // [{}, {}, {}]
 
 const arr = [1, 2, 3, 4, 5]
+
+for(const num of arr){
+//    console.log(num);
+}
+
+const greeting = "Hello world!"
+for(const greet of greeting){
+    // console.log(`Each char is ${greet}`);
+}
+
+// Maps
+
+const map = new Map()
+map.set(`IN`, "India")
+map.set(`USA`, "United States of America")
+map.set(`Fr`, "France")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, `:-`, value);
+}
+
+const myobject = {
+    game1: 'NFS',
+    game2: 'spiderman'
+}
+
+// for (const [key, value] of myobject) {
+//     console.log(key, `:-`, value);
+// }
